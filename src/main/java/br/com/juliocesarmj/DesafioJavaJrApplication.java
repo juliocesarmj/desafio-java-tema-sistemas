0afbf45f5ce06de74127d234a3bf37edde47393d
juliocesarmj <juliocesardemoraes2015@gmail.com>
2021-08-27 11:57:44 -0300

    configuracoes iniciais do projeto

---
+++ src/main/java/br/com/juliocesarmj/DesafioJavaJrApplication.java
@@ -0,0 +1,13 @@
+package br.com.juliocesarmj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJavaJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioJavaJrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/juliocesarmj/DesafioJavaJrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juliocesarmj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJavaJrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
